3c614f0a8f194c726f9ec90de0f6ae045fc6ef11
vincentpham123 <vincentpham32@gmail.com>
2023-06-19 18:32:54 -0700

    deployment pt 3

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,4 @@
-class StaticPagesController < ApplicationController
+class StaticPagesController < ApplicationController::Base
     def frontend_index
         render file: Railes.root.join('publc','index.html')
     end
